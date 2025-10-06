2f01af2c827e8f8788513b6f8da58b07f9cb5885
Siddharth Thakur <u803760@gmail.com>
2025-10-06 15:20:34 +0530

    made frontend fixes

--- frontend/src/components/Profile.tsx
+++ frontend/src/components/Profile.tsx
@@ -3,7 +3,7 @@ import { CgProfile } from "react-icons/cg";
 const Profile = () => {
   return (
     <div>
-      <CgProfile className="h-8 w-8"/>
+      <CgProfile className="h-8 w-8 cursor-pointer "/>
     </div>
   );
 };
